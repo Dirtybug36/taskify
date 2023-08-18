048b812c4ea9d10f2a755cdfcc96947af6529b45
Dirtybug36 <104801789+Dirtybug36@users.noreply.github.com>
2023-08-18 01:17:54 +0100

    handle delete and handle done

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -10,19 +10,32 @@ interface Props {
   tasks: Todo[];
 }
 export const SingleTask = ({ setTasks, isDone, id, task, tasks }: Props) => {
+  const handleDone = (id: number) => {
+    setTasks(tasks.map((e) => (e.id === id ? { ...e, isDone: !e.isDone } : e)));
+  };
+  const handleDelete = (id: number) => {
+    setTasks(tasks.filter((e) => e.id !== id));
+  };
   return (
     <form className="flex w-[30%] rounded-md p-5 mt-[15px] items-center bg-[url(https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg)] max-[700px]:w-[100%]">
-      <span className="flex-1 p-1 border-none text-xl focus:outline-none">
-        {task}
-      </span>
+      {!isDone ? (
+        <span className="flex-1 p-1 border-none text-xl focus:outline-none">
+          {task}
+        </span>
+      ) : (
+        <s className="flex-1 p-1 border-none text-xl focus:outline-none">
+          {task}
+        </s>
+      )}
+
       <span className="ml-[10px] text-2xl cursor-pointer">
         <AiFillEdit />
       </span>
       <span className="ml-[10px] text-2xl cursor-pointer">
-        <AiFillDelete />
+        <AiFillDelete onClick={() => handleDelete(id)} />
       </span>
       <span className="ml-[10px] text-2xl cursor-pointer">
-        <MdDone />
+        <MdDone onClick={() => handleDone(id)} />
       </span>
     </form>
   );
